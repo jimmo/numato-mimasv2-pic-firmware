4673bf23be6a0d9a7925e8e1b8466f7cd2585640
Jim Mussared <jim.mussared@gmail.com>
2017-09-08 17:51:40 +1000

    Update programmer.py to detect chip id.

--- programmer.py
+++ programmer.py
@@ -1,26 +1,37 @@
 import serial
-import xmodem
 import sys
+import xmodem
 
-port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-port.write(b'\r\n\r\n')
-if port.in_waiting:
-  port.read(port.in_waiting)
+
+if len(sys.argv) != 2:
+  print('Usage: {} filename.bin'.format(sys.argv[0]))
+  sys.exit(1)
+
+port = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
+port.write(b'\x03\r\n\r\n')
+port.read(100)
 port.write(b'i\r')
-print(port.read(100))
+chipid = port.read(100).decode()
+chipid = chipid.split('\n')
+if len(chipid) > 1 and chipid[1].strip() == '202015':
+  print('Detected Mimas v2 with M25P16 SPI Flash.')
+else:
+  sys.exit(1)
 
 def xmodem_getc(size, timeout=1):
+  port.timeout = timeout
   r = port.read(size) or None
-  #print('Rx:', r)
   return r
 
 def xmodem_putc(data, timeout=1):
-  #print('Tx:', data)
+  port.timeout = timeout
   return port.write(data)
 
+print('Sending {}'.format(sys.argv[1]))
 modem = xmodem.XMODEM(xmodem_getc, xmodem_putc)
 port.write(b'f\r')
 port.read(3)
 
 stream = open(sys.argv[1], 'rb')
 modem.send(stream)
+print('Done.')
