68cbbf1a7e94acbfb3188d5a5e0f582c14a6d28c
Jim Mussared <jim.mussared@gmail.com>
2017-09-09 16:11:41 +1000

    Update programmer.py to make verification optional.

--- programmer.py
+++ programmer.py
@@ -1,13 +1,21 @@
+import argparse
 import serial
 import sys
 import xmodem
 
+parser = argparse.ArgumentParser(description='Mimas V2 Programmer')
+parser.add_argument('--uart', help='Path to UART device', default='/dev/ttyACM0')
+parser.add_argument('--baudrate', help='Baudrate of UART device', default=115200, type=int)
+parser.add_argument('--filename', help='Full path to binary file', default='build/mimasv2_base_lm32/flash.bin')
+verify_parser = parser.add_mutually_exclusive_group(required=False)
+verify_parser.add_argument('--verify', dest='verify', action='store_true')
+verify_parser.add_argument('--no-verify', dest='verify', action='store_false')
+parser.set_defaults(verify=True)
+parser.add_argument('--protocol', help='XMODEM protocol (xmodem or xmodem1k)', default='xmodem1k')
+args = parser.parse_args()
 
-if len(sys.argv) != 2:
-  print('Usage: {} filename.bin'.format(sys.argv[0]))
-  sys.exit(1)
+port = serial.Serial(args.uart, args.baudrate, timeout=0.1)
 
-port = serial.Serial('/dev/ttyACM0', 115200, timeout=0.1)
 port.write(b'\x03\r\n\r\n')
 port.read(100)
 port.write(b'i\r')
@@ -16,7 +24,15 @@ chipid = chipid.split('\n')
 if len(chipid) > 1 and chipid[1].strip() == '202015':
   print('Detected Mimas v2 with M25P16 SPI Flash.')
 else:
+  print('Didn\'t find SPI Flash.')
   sys.exit(1)
+
+if args.verify:
+  port.write(b'C\r')
+else:
+  print('Warning: disabling verification.')
+  port.write(b'c\r')
+port.read(len('x\r\nmimas> '))
 
 def xmodem_getc(size, timeout=1):
   port.timeout = timeout
@@ -29,11 +45,13 @@ def xmodem_putc(data, timeout=1):
   #print('Tx:', data)
   return port.write(data)
 
-print('Sending {}'.format(sys.argv[1]))
-modem = xmodem.XMODEM(xmodem_getc, xmodem_putc, mode='xmodem1k')
+print('Sending {}'.format(args.filename))
+modem = xmodem.XMODEM(xmodem_getc, xmodem_putc, mode=args.protocol)
 port.write(b'f\r')
 port.read(3)
 
-stream = open(sys.argv[1], 'rb')
-modem.send(stream)
-print('Done.')
+with open(args.filename, 'rb') as stream:
+  if modem.send(stream):
+    print('Done.')
+  else:
+    print('Failed.')
